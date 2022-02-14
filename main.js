b9a340ecf27fd345179a3806538813e0a9ae7b7b
Roman <7243180roma65@gmail.com>
2022-02-14 19:43:44 +0300

    Всё понял и разобрался

--- main.js
+++ main.js
@@ -22,40 +22,35 @@ const player2 = {
     }
 };
 
-function createPlayer(player, name, hp){
+function createPlayer(player, obj, hp){
     const $player = document.createElement('div');
-    $player.classList.add('player1');                        // Немного не понял как сделать так, чтобы создавалось два разных класса из одной строки.
-                                                             // Тоесть как передать объект player2 так, чтобы для него создался класс (есть подозрения, что нужно имя входящего объекта преобразовать через toString,
-                                                             // но в js я не знаю как это реализовать)
+    $player.classList.add(player);                        
+
         const $progressbar = document.createElement('div')
         $progressbar.classList.add('progressbar');
 
             const $life = document.createElement('div')
             $life.classList.add('life');
-                const $p_hp = document.createElement('p');
-                $p_hp.innerText = hp;
+                $life.style.width = hp + '%';
 
             const $name = document.createElement('div')
             $name.classList.add('name');
-                const $p_name = document.createElement('p');
-                $p_name.innerText = name;
+            $name.innerText = obj.name;
 
         const $character = document.createElement('div')
         $character.classList.add('character');
 
             const $img = document.createElement('img')
-            $img.src = player.img;
+            $img.src = obj.img;
 
     const $root = document.querySelector('.arenas');
     $root.appendChild($player);
         $player.appendChild($progressbar);
             $progressbar.appendChild($life);
-               $life.appendChild($p_hp);
             $progressbar.appendChild($name);
-               $name.appendChild($p_name);
         $player.appendChild($character);
             $character.appendChild($img);
 };
 
-createPlayer(player1, player1.name, 100);
-createPlayer(player2, player2.name, 100);
+createPlayer('player1', player1, 100);
+createPlayer('player2', player2, 100);
